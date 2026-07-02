d4e9f5e305950201f4120f1349d28a6e61dd941a
Bagus Fatihuddin <bagusfatihuddin@gmail.com>
2026-07-03 02:18:36 +0800

    meta data dan favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @viteReactRefresh
+    <title>Karang Sidemen</title>
 
+    <meta name="description" content="Website resmi Desa Wisata Karang Sidemen.">
+
+    <meta name="theme-color" content="#0D5C3D">
+
+    <meta property="og:title" content="Karang Sidemen">
+    <meta property="og:description" content="Website resmi Desa Wisata Karang Sidemen.">
+    <meta property="og:type" content="website">
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
+
+    @viteReactRefresh
     @vite('resources/js/react/main.jsx')
 </head>
 <body>
